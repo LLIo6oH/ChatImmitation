8f3d2a4daa933509d88793ebadc5a0f85ac2437b
Dmitry Kuklin <dmi-kuklin@yandex.ru>
2020-09-15 02:19:44 +0300

    cleaning

--- ChatImitation/Scenes/DialogueView.swift
+++ ChatImitation/Scenes/DialogueView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct DialogueView: View {
     
     @ObservedObject var dialogViewModel = DialogueViewModel()
-    @State var opacity: Double = 0
-    @State var showedMessageIndex = 1
+    @State var showedMessageIndex = 0
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
     
     private var chatBubble: some View {
         RoundedRectangle(cornerRadius: 5)
@@ -32,9 +31,24 @@ struct DialogueView: View {
             .frame(width: width, height: height)
             .shadow(color: Color.black.opacity(0.5), radius: 1, x: 0, y: 1)
             .zIndex(10)
-//            .clipped()
+            .clipped()
             .padding(.trailing, -1)
-            .padding(.bottom, 0)
+    }
+    
+    private func showDialogueLine(text: String) -> some View {
+        HStack(alignment: .bottom, spacing: 0) {
+            chatBubbleTriange(width: 11, height: 18)
+            
+            Text(text)
+                .font(.system(size: 17.0, weight: .light))
+                .foregroundColor(Color.black)
+                .padding(10)
+                .background(chatBubble)
+            
+            Spacer()
+        }
+        .padding(.leading, 20)
+        .padding(.top, 30)
     }
     
     var body: some View {
@@ -54,34 +68,22 @@ struct DialogueView: View {
 
                                 ForEach(dialogViewModel.dialogueLines, id: \.self) { dialogueLine in
                                     
-                                    HStack(alignment: .bottom, spacing: 0) {
-                                        chatBubbleTriange(width: 11, height: 18)
-                                        
-                                        Text(dialogueLine.line)
-                                            .font(.system(size: 17.0, weight: .light))
-                                            .foregroundColor(Color.black)
-                                            .padding(10)
-                                            .background(chatBubble)
-                                        
-                                        Spacer()
-                                    }
-                                    .padding(.leading, 20)
+                                showDialogueLine(text: dialogueLine.line)
                                     .padding(.trailing, geometry.size.width * 0.15)
-                                    .padding(.top, 30)
                                 }
-                                .padding(.top, geometry.size.height - 100)
+                                .padding(.top, geometry.size.height)
                                 .background(Color(hex: AppDefaults.AppColors.mainBgColor))
                                 .navigationBarTitle(Text("Dialogue"), displayMode: .inline)
-                                .onAppear() {
-                                    dialogViewModel.getDialogue()
-                                }
                             }
                             .background(Color(hex: AppDefaults.AppColors.mainBgColor))
+                            .onAppear() {
+                                dialogViewModel.getDialogue()
+                            }
                             .onReceive(timer) { _ in
                                 withAnimation(.easeInOut(duration: 0.5)) {
                                     if showedMessageIndex < dialogViewModel.dialogueLines.count {
-                                    scrollProxy.scrollTo(dialogViewModel.dialogueLines[showedMessageIndex], anchor: .bottom)
-                                    showedMessageIndex += 1
+                                        scrollProxy.scrollTo(dialogViewModel.dialogueLines[showedMessageIndex], anchor: .bottom)
+                                        showedMessageIndex += 1
                                     } else {
                                         timer.upstream.connect().cancel()
                                     }
@@ -100,6 +102,5 @@ struct DialogueView_Previews: PreviewProvider {
         Group {
             DialogueView()
         }
-        
     }
 }
